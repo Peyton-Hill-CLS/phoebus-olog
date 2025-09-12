1ac1507a6aab38196a111d8406ba9e51ec1f755b
hillp <peyton.hill@lightsource.ca>
2025-09-12 11:15:52 -0600

    Did some hacky workarounds to possibly get this to work though both java SPI and spring beans.

--- src/main/java/org/phoebus/olog/notification/EmailLogEntryNotifier.java
+++ src/main/java/org/phoebus/olog/notification/EmailLogEntryNotifier.java
@@ -14,86 +14,101 @@ import org.simplejavamail.api.mailer.config.TransportStrategy;
 import org.simplejavamail.email.EmailBuilder;
 import org.simplejavamail.mailer.MailerBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-@Configuration
-@Component
+
 public class EmailLogEntryNotifier implements LogEntryNotifier {
 
-    private final Logger logger = Logger.getLogger(EmailLogEntryNotifier.class.getName());
-
-    public Mailer mailer;
-
-    public final Parser parser;
-    public final HtmlRenderer renderer;
-
-    @Value("${email.url}")
-    public String url;
-    @Value("${email.host}")
-    public String host;
-    @Value("${email.port:25}")
-    public int port;
-    @Value("${email.send.address}")
-    public String address;
+    private final SpringEmailLogEntryNotifier child;
 
     public EmailLogEntryNotifier() {
-        List<Extension> extensions = List.of(TablesExtension.create());
-        parser = Parser.builder().extensions(extensions).build();
-        renderer = HtmlRenderer.builder().extensions(extensions).build();
-
-        logger.log(Level.INFO, "Starting email notifier");
-    }
-
-    @PostConstruct
-    public void initMailer() {
-        logger.info("Initializing mailer with host: " + host + ", port: " + port);
-
-        mailer = MailerBuilder
-                .withSMTPServer(host, port)
-                .withTransportStrategy(TransportStrategy.SMTP_TLS)
-                .buildMailer();
-
-        System.setProperty("mail.smtp.starttls.enable", "false");
-        System.setProperty("mail.smtp.ssl.trust", host);
+        ApplicationContext context = new AnnotationConfigApplicationContext();
+        this.child = context.getBean(SpringEmailLogEntryNotifier.class);
     }
 
     @Override
     public void notify(Log logEntry) {
-        if(logEntry.getForwardTo().isEmpty()) {
-            return;
-        }
-        EmailPopulatingBuilder builder = EmailBuilder.startingBlank();
-
-        for(String s : logEntry.getForwardTo()) {
-            builder = builder.to(s);
-        }
-
-        StringBuilder content = new StringBuilder();
-        content.append("User: ").append(logEntry.getOwner());
-        content.append("\\\nDate: ").append(logEntry.getCreatedDate());
-        content.append("\\\nSummary: ").append(logEntry.getTitle());
-        content.append("\\\nDetails: \n\n").append(logEntry.getSource());
-        content.append("\n\nView this log at: [").append(url).append(logEntry.getId()).append("](").append(url).append(logEntry.getId()).append(")");
-
-        final Email email = builder
-                .from(address)
-                .withSubject("Olog Entry: " + logEntry.getTitle())
-                .withHTMLText(convertMarkdownToHtml(content.toString()))
-                .buildEmail();
-
-
-        logger.log(Level.INFO, "Sending email");
-        mailer.sendMail(email);
-        logger.log(Level.INFO, "Email sent");
+        child.notify(logEntry);
     }
 
-    public String convertMarkdownToHtml(String md) {
-        Node document = parser.parse(md);
-        return renderer.render(document);
+    @Configuration
+    public static class SpringEmailLogEntryNotifier implements LogEntryNotifier {
+        private final Logger logger = Logger.getLogger(EmailLogEntryNotifier.class.getName());
+
+        public Mailer mailer;
+
+        public final Parser parser;
+        public final HtmlRenderer renderer;
+
+        @Value("${email.url}")
+        public String url;
+        @Value("${email.host}")
+        public String host;
+        @Value("${email.port:25}")
+        public int port;
+        @Value("${email.send.address}")
+        public String address;
+
+        public SpringEmailLogEntryNotifier() {
+            List<Extension> extensions = List.of(TablesExtension.create());
+            parser = Parser.builder().extensions(extensions).build();
+            renderer = HtmlRenderer.builder().extensions(extensions).build();
+
+            logger.log(Level.INFO, "Starting email notifier");
+        }
+
+        @PostConstruct
+        public void initMailer() {
+            logger.info("Initializing mailer with host: " + host + ", port: " + port);
+
+            mailer = MailerBuilder
+                    .withSMTPServer(host, port)
+                    .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                    .buildMailer();
+
+            System.setProperty("mail.smtp.starttls.enable", "false");
+            System.setProperty("mail.smtp.ssl.trust", host);
+        }
+
+        @Override
+        public void notify(Log logEntry) {
+            if (logEntry.getForwardTo().isEmpty()) {
+                return;
+            }
+            EmailPopulatingBuilder builder = EmailBuilder.startingBlank();
+
+            for (String s : logEntry.getForwardTo()) {
+                builder = builder.to(s);
+            }
+
+            StringBuilder content = new StringBuilder();
+            content.append("User: ").append(logEntry.getOwner());
+            content.append("\\\nDate: ").append(logEntry.getCreatedDate());
+            content.append("\\\nSummary: ").append(logEntry.getTitle());
+            content.append("\\\nDetails: \n\n").append(logEntry.getSource());
+            content.append("\n\nView this log at: [").append(url).append(logEntry.getId()).append("](").append(url).append(logEntry.getId()).append(")");
+
+            final Email email = builder
+                    .from(address)
+                    .withSubject("Olog Entry: " + logEntry.getTitle())
+                    .withHTMLText(convertMarkdownToHtml(content.toString()))
+                    .buildEmail();
+
+
+            logger.log(Level.INFO, "Sending email");
+            mailer.sendMail(email);
+            logger.log(Level.INFO, "Email sent");
+        }
+
+        public String convertMarkdownToHtml(String md) {
+            Node document = parser.parse(md);
+            return renderer.render(document);
+        }
     }
 }
